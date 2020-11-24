792c9b8b7fedd2c641cd4f55d58ff61c2f194aeb
FRIEDRICH505 <67607043+FRIEDRICH505@users.noreply.github.com>
2020-11-24 19:08:25 +0200

    Add files via upload

---
+++ tiy-17-3.py
@@ -0,0 +1,106 @@
+import requests
+
+from plotly.graph_objs import Bar
+from plotly import offline
+
+def get_response():
+    """Make an api call, and return the response."""
+    url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+    headers = {'Accept': 'application/vnd.github.v3+json'}
+    r = requests.get(url, headers=headers)
+    return r
+
+def get_repo_dicts(r):
+    """Return a set of dicts representing the most popular repositories."""
+    response_dict = r.json()
+    repo_dicts = response_dict['items']
+    return repo_dicts
+
+def get_project_data(repo_dicts):
+    """Return data needed for each project in visualization."""
+    repo_links, stars, labels = [], [], []
+    for repo_dict in repo_dicts:
+        repo_name = repo_dict['name']
+        repo_url = repo_dict['html_url']
+        repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+        repo_links.append(repo_link)
+
+        stars.append(repo_dict['stargazers_count'])
+
+        owner = repo_dict['owner']['login']
+        description = repo_dict['description']
+        label = f"{owner}<br />{description}"
+        labels.append(label)
+
+    return repo_links, stars, labels
+
+def make_visualization(repo_links, stars, labels):
+    """Generate the visualization of most commented articles."""
+    data = [{
+        'type': 'bar',
+        'x': repo_links,
+        'y': stars,
+        'hovertext': labels,
+        'marker': {
+            'color': 'rgb(60, 100, 150)',
+            'line': {'width': 1.5, 'color': 'rgb(25, 25, 25)'}
+        },
+        'opacity': 0.6,
+    }]
+
+    my_layout = {
+        'title': 'Most-Starred Python Projects on GitHub',
+        'titlefont': {'size': 28},
+        'xaxis': {
+            'title': 'Repository',
+            'titlefont': {'size': 24},
+            'tickfont': {'size': 14},
+        },
+        'yaxis': {
+            'title': 'Stars',
+            'titlefont': {'size': 24},
+            'tickfont': {'size': 14},
+        },
+
+    }
+
+    fig = {'data': data, 'layout': my_layout}
+    offline.plot(fig, filename='python_repos.html')
+
+if __name__ == '__main__':
+    r = get_response()
+    repo_dicts = get_repo_dicts(r)
+    repo_links, stars, labels = get_project_data(repo_dicts)
+    make_visualization(repo_links, stars, labels)
+
+import unittest
+
+import python_repos as pr
+
+class PythonReposTestCase(unittest.TestCase):
+    """Tests for python_repos.py."""
+
+    def setUp(self):
+        """Call all the functions here, and test elements separately."""
+        self.r = pr.get_response()
+        self.repo_dicts = pr.get_repo_dicts(self.r)
+        self.repo_dict = self.repo_dicts[0]
+        self.repo_links, self.stars, self.labels = pr.get_project_data(
+                self.repo_dicts)
+
+    def test_get_response(self):
+        """Test that we get a valid response."""
+        self.assertEqual(self.r.status_code, 200)
+
+    def test_repo_dicts(self):
+        """Test that we're getting the data we think we are."""
+        # We should get dicts for 30 repositories.
+        self.assertEqual(len(self.repo_dicts), 30)
+
+        # Repositories should have required keys.
+        required_keys = ['name', 'owner', 'stargazers_count', 'html_url']
+        for key in required_keys:
+            self.assertTrue(key in self.repo_dict.keys())
+
+if __name__ == '__main__':
+    unittest.main()
